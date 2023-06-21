54ef4500df4d2668943517369a0a806674f04d9f
Roman__Mal'cev <61384075+DevFromSiberia@users.noreply.github.com>
2023-06-21 22:18:00 +0700

    Added result page layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import { ResultPage } from './pages/ResultPage';
 import './styles/App.css';
-
+const fakeArr = [1, 2, 3, 4]
 function App() {
   return (
     <div className="App">
-      Hello
+      <ResultPage data={fakeArr} />
     </div>
   );
 }
