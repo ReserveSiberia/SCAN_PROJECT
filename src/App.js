42c4671ad858b08c555291345da41f9d1e91fa9d
SashaTpi <tpilampy@yandex.ru>
2023-06-25 17:31:53 +0500

    final

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import './styles/App.css';
-const fakeArr = [1, 2, 3, 4]
-import { SearchPage } from './pages/SearchPage';
+import "./styles/App.css";
+import MainPage from "./pages/MainPage/MainPage.jsx";
+import Header from "./components/Header/Header.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import { ResultPage } from './pages/ResultPage';
+import { Navigate, Outlet, Route, Routes } from 'react-router-dom';
+import { SearchPage } from "./pages/SearchPage";
+import './styles/variables.css';
 
+
+const fakeArr = [1, 2, 3, 4]
 function App() {
+  const auth = true
   return (
     <div className="App">
-      Hello
+
+      <Header />
+      <Routes>
+        <Route path='/' element={<MainPage />} />
+        <Route path='/auth' element={<>AuthPage</>} />
+        {/* Если пользователь не авторизован то редиректит на форму авторизации, иначе есть доступ к поиску и результатам */}
+        <Route element={auth ? <Outlet /> : <Navigate to="/auth" />}>
+          <Route path='/search' element={<SearchPage/>} />
+          <Route path='/result' element={<ResultPage data={fakeArr} />} />
+        </Route>
+      </Routes>
+      <Footer />
+
     </div>
   );
 }
+
+export default App;
 
--- src/pages/SearchPage/index.jsx
+++ src/pages/SearchPage/index.jsx
@@ -1,11 +1,5 @@
 import React, { useState } from 'react';
-<<<<<<< HEAD
-import './styles/variables.css';
 import styles from './SearchPage.module.css';
-=======
-import '../../styles/variables.css';
-import './SearchPage.module.css';
->>>>>>> 5de5b35de77fddf828894fca6d8375466d6a9ba1
 import DocumentImage from '../../assets/images/SearchPageImg3.svg';
 import FolderImage from '../../assets/images/SearchPageImg2.svg';
 import GroupImage from '../../assets/images/SearchPageImg1.svg';
@@ -51,177 +45,147 @@ const SearchPage = () => {
   };
 
   return (
-<<<<<<< HEAD
-    <div className={styles.searchPage}>
-      <div className={styles.imageContainer}>
-      <img src={DocumentImage} alt="Search Page" className={styles.imageDoc} />
-      </div>
-      <div className={styles.imageContainer}>
-      <img src={FolderImage} alt="Search Page" className={styles.imageFol} />
-      </div>
-      <div className={styles.imageContainer}>
-      <img src={GroupImage} alt="Search Page" className={styles.imageGro} />
-=======
-    <div className="search-page">
-      <div className="search-page__image-container">
-        <img src={DocumentImage} alt="Search Page" className="Document__image" />
-      </div>
-      <div className="search-page__image-container">
-        <img src={FolderImage} alt="Search Page" className="Folder__image" />
-      </div>
-      <div className="search-page__image-container">
-        <img src={GroupImage} alt="Search Page" className="Group__image" />
->>>>>>> 5de5b35de77fddf828894fca6d8375466d6a9ba1
+    <main className={styles.searchPage}>
+       <div className={styles.imageContainer}>
+        <img src={DocumentImage} alt="Search Page" className={styles.imageDoc} />
+        <img src={FolderImage} alt="Search Page" className={styles.imageFol} />
+        <img src={GroupImage} alt="Search Page" className={styles.imageGro} />
       </div>
       <h1 className={styles.title}>НАЙДИТЕ НЕОБХОДИМЫЕ ДАННЫЕ В ПАРУ КЛИКОВ</h1>
       <p>Задайте параметры поиска. Чем больше заполните, тем точнее поиск</p>
-      <form>
-        <div className={styles.formGroup}>
-          <label htmlFor="inn" className={styles.label}>ИНН Компани*</label>
-          <input
-            type="text"
-            id="inn"
-            name="inn"
-            value={searchData.inn}
-            onChange={handleInputChange}
-            required
-            className={styles.input}
-          />
-        </div>
-        <div className={styles.formGroup}>
-          <input
-            type="checkbox"
-            id="completeness"
-            name="completeness"
-            checked={searchData.completeness}
-            onChange={handleInputChange}
-            className={styles.checkbox}
-          />
-          <label htmlFor="completeness" className={styles.label}>Признак максимальной полноты</label>
-        </div>
-        <div className={styles.formGroup}>
-          <input
-            type="checkbox"
-            id="businessContext"
-            name="businessContext"
-            checked={searchData.businessContext}
-            onChange={handleInputChange}
-            className={styles.checkbox}
-          />
-          <label htmlFor="businessContext" className={styles.label}>Упоминания в бизнес-контексте</label>
-        </div>
-        <div className={styles.formGroup}>
-          <input
-            type="checkbox"
-            id="mainRole"
-            name="mainRole"
-            checked={searchData.mainRole}
-            onChange={handleInputChange}
-            className={styles.checkbox}
-          />
+        <form className={styles.form}>
+          <div className={styles.left}>
+            <label htmlFor="inn" className={styles.label}>ИНН Компани*</label>
+            <input
+              type="text"
+              id="inn"
+              name="inn"
+              value={searchData.inn}
+              onChange={handleInputChange}
+              required
+              className={styles.input}
+            />
+            <label htmlFor="documentCount" className={styles.label}>Количество документов в выдаче*</label>
+            <input
+              type="number"
+              id="documentCount"
+              name="documentCount"
+              value={searchData.documentCount}
+              onChange={handleInputChange}
+              required
+              className={styles.input}
+            />
+            <label htmlFor="startDate" className={styles.label}>Дата начала поиска*</label>
+            <input
+              type="date"
+              id="startDate"
+              name="startDate"
+              value={searchData.startDate}
+              onChange={handleInputChange}
+              required
+              className={styles.input}
+            />
+            <label htmlFor="endDate" className={styles.label}>Дата конца поиска*</label>
+            <input
+              type="date"
+              id="endDate"
+              name="endDate"
+              value={searchData.endDate}
+              onChange={handleInputChange}
+              required
+              className={styles.input}
+            />
+            </div>
+          <div className={styles.right}>
+            <label htmlFor="completeness" className={styles.label}>Признак максимальной полноты</label>
+            <input
+              type="checkbox"
+              id="businessContext"
+              name="businessContext"
+              checked={searchData.businessContext}
+              onChange={handleInputChange}
+              className={styles.checkbox}
+            />
+            <label htmlFor="businessContext" className={styles.label}>Упоминания в бизнес-контексте</label>
+            <input
+              type="checkbox"
+              id="mainRole"
+              name="mainRole"
+              checked={searchData.mainRole}
+              onChange={handleInputChange}
+              className={styles.checkbox}
+            />
             <label htmlFor="mainRole" className={styles.label}>Главная роль в публикации</label>
-        </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="tonality" className={styles.label}>Тональность*</label>
-          <select
-            id="tonality"
-            name="tonality"
-            value={searchData.tonality}
-            onChange={handleInputChange}
-            required
-            className={styles.select}
-          >
-            <option value="positive">Позитивная</option>
-            <option value="negative">Негативная</option>
-            <option value="any">Любая</option>
-          </select>
-        </div>
-        <div className={styles.formGroup}>
-          <input
-            type="checkbox"
-            id="riskFactors"
-            name="riskFactors"
-            checked={searchData.riskFactors}
-            onChange={handleInputChange}
-            className={styles.checkbox}
-          />
-          <label htmlFor="riskFactors" className={styles.label}>Публикации только с риск-факторами</label>
-        </div>
-        <div className={styles.formGroup}>
-          <input
-            type="checkbox"
-            id="technicalNews"
-            name="technicalNews"
-            checked={searchData.technicalNews}
-            onChange={handleInputChange}
-            className={styles.checkbox}
-          />
-          <label htmlFor="technicalNews" className={styles.label}>Включать технические новости рынков</label>
-        </div>
-        <div className={styles.formGroup}>
-          <input
-            type="checkbox"
-            id="announcements"
-            name="announcements"
-            checked={searchData.announcements}
-            onChange={handleInputChange}
-            className={styles.checkbox}
-          />
-          <label htmlFor="announcements" className={styles.label}>Включать анонсы и календари</label>
-        </div>
-        <div className={styles.formGroup}>
-          <input
-            type="checkbox"
-            id="newsDigests"
-            name="newsDigests"
-            checked={searchData.newsDigests}
-            onChange={handleInputChange}
-            className={styles.checkbox}
-          />
-          <label htmlFor="newsDigests" className={styles.label}>Включать сводки новостей</label>
-        </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="documentCount" className={styles.label}>Количество документов в выдаче*</label>
-          <input
-            type="number"
-            id="documentCount"
-            name="documentCount"
-            value={searchData.documentCount}
-            onChange={handleInputChange}
-            required
-            className={styles.input}
-          />
-        </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="startDate" className={styles.label}>Дата начала поиска*</label>
-          <input
-            type="date"
-            id="startDate"
-            name="startDate"
-            value={searchData.startDate}
-            onChange={handleInputChange}
-            required
-            className={styles.input}
-          />
-        </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="endDate" className={styles.label}>Дата конца поиска*</label>
-          <input
-            type="date"
-            id="endDate"
-            name="endDate"
-            value={searchData.endDate}
-            onChange={handleInputChange}
-            required
-            className={styles.input}
-          />
-        </div>
-        <button type="button" onClick={handleSearch} disabled={!isFormValid()} className={styles.submitButton}>
-          Поиск
-        </button>
-      </form>
-    </div>
-  );
-};
+            <input
+              type="checkbox"
+              id="completeness"
+              name="completeness"
+              checked={searchData.completeness}
+              onChange={handleInputChange}
+              className={styles.checkbox}
+              />
+            <label htmlFor="tonality" className={styles.label}>Тональность*</label>
+            <select
+              id="tonality"
+              name="tonality"
+              value={searchData.tonality}
+              onChange={handleInputChange}
+              required
+              className={styles.select}
+            >
+              <option value="positive">Позитивная</option>
+              <option value="negative">Негативная</option>
+              <option value="any">Любая</option>
+            </select>
+            <input
+              type="checkbox"
+              id="riskFactors"
+              name="riskFactors"
+              checked={searchData.riskFactors}
+              onChange={handleInputChange}
+              className={styles.checkbox}
+            />
+            <label htmlFor="riskFactors" className={styles.label}>Публикации только с риск-факторами</label>
+
+            <input
+              type="checkbox"
+              id="technicalNews"
+              name="technicalNews"
+              checked={searchData.technicalNews}
+              onChange={handleInputChange}
+              className={styles.checkbox}
+            />
+            <label htmlFor="technicalNews" className={styles.label}>Включать технические новости рынков</label>
+
+            <input
+              type="checkbox"
+              id="announcements"
+              name="announcements"
+              checked={searchData.announcements}
+              onChange={handleInputChange}
+              className={styles.checkbox}
+            />
+            <label htmlFor="announcements" className={styles.label}>Включать анонсы и календари</label>
+  
+            <input
+              type="checkbox"
+              id="newsDigests"
+              name="newsDigests"
+              checked={searchData.newsDigests}
+              onChange={handleInputChange}
+              className={styles.checkbox}
+            />
+            <label htmlFor="newsDigests" className={styles.label}>Включать сводки новостей</label>
+            </div> 
+                <button type="button" onClick={handleSearch} disabled={!isFormValid()} className={styles.submitButton}>
+            Поиск
+            {!isFormValid() && (
+            <div className={styles.error}>Пожалуйста, заполните все поля формы.</div>
+            )}
+          </button>
+        </form>
+      </main>
+    );
+  };
 
 export { SearchPage };
