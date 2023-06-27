95a5f640678d0817e99e1575e480f823c241d7c8
Roman__Mal'cev <61384075+DevFromSiberia@users.noreply.github.com>
2023-06-27 19:57:36 +0700

    merge

--- src/pages/AuthPage/index.jsx
+++ src/pages/AuthPage/index.jsx
@@ -14,90 +14,87 @@ import styles from "./Auth.module.css";
 import styles from "./Auth.module.css";
 
 const Auth = () => {
-  const Auth = () => {
-    return (
-      <Container>
-        <div className={styles.displayForm}>
-          <div className={styles.Authorization}>
-            <h1>
-              ДЛЯ ОФОРМЛЕНИЯ ПОДПИСКИ
-              <br />
-              НА ТАРИФ, НЕОБХОДИМО
-              <br />
-              АВТОРИЗОВАТЬСЯ.
-            </h1>
-            <img
-              className={styles.Characters}
-              src={Characters}
-              alt="Characters"
-            />
-          </div>
-          <div className={styles.form}>
-            <div className={styles.loginContainer}>
-              <div className={styles.row}>
-                <div className={styles.loginForm}>
-                  <form>
-                    <img className={styles.lock} src={lock} alt="lock" />
-                    <div className={styles.loginsignup}>
-                      <Button className={styles.login}>Войти</Button>
-                      <Button className={styles.signup}>
-                        Зарегистрироваться
-                      </Button>
+  return (
+    <Container>
+      <div className={styles.displayForm}>
+        <div className={styles.Authorization}>
+          <h1>
+            ДЛЯ ОФОРМЛЕНИЯ ПОДПИСКИ
+            <br />
+            НА ТАРИФ, НЕОБХОДИМО
+            <br />
+            АВТОРИЗОВАТЬСЯ.
+          </h1>
+          <img
+            className={styles.Characters}
+            src={Characters}
+            alt="Characters"
+          />
+        </div>
+        <div className={styles.form}>
+          <div className={styles.loginContainer}>
+            <div className={styles.row}>
+              <div className={styles.loginForm}>
+                <form>
+                  <img className={styles.lock} src={lock} alt="lock" />
+                  <div className={styles.loginsignup}>
+                    <Button className={styles.login}>Войти</Button>
+                    <Button className={styles.signup}>
+                      Зарегистрироваться
+                    </Button>
+                  </div>
+                  <div className={styles.formGroup}>
+                    <label>
+                      Логин или номер телефона:
+                      <input
+                        type="text"
+                        className={styles.FormControl}
+                        placeholder=""
+                      />
+                    </label>
+                  </div>
+                  <div className={styles.formGroup}>
+                    <label>
+                      Пароль:
+                      <input
+                        type="password"
+                        className={styles.FormControl}
+                        placeholder=""
+                      />
+                    </label>
+                  </div>
+                  <Button className={styles.btnSubmit}>Войти</Button>
+                  <div className={styles.formGroup}>
+                    <a href="#" className={styles.recoverPwd}>
+                      Восстановить пароль
+                    </a>
+                    <label>Войти через:</label>
+                    <div className={styles.imgsvg}>
+                      <img
+                        className={styles.Google}
+                        src={Google}
+                        alt="Geogle"
+                      />
+                      <img
+                        className={styles.facebook}
+                        src={facebook}
+                        alt="facebook"
+                      />
+                      <img
+                        className={styles.yandex}
+                        src={yandex}
+                        alt="yandex"
+                      />
                     </div>
-                    <div className={styles.formGroup}>
-                      <label>
-                        Логин или номер телефона:
-                        <input
-                          type="text"
-                          className={styles.FormControl}
-                          placeholder=""
-                        />
-                      </label>
-                    </div>
-                    <div className={styles.formGroup}>
-                      <label>
-                        Пароль:
-                        <input
-                          type="password"
-                          className={styles.FormControl}
-                          placeholder=""
-                        />
-                      </label>
-                    </div>
-                    <Button className={styles.btnSubmit}>Войти</Button>
-                    <div className={styles.formGroup}>
-                      <a href="#" className={styles.recoverPwd}>
-                        Восстановить пароль
-                      </a>
-                      <label>Войти через:</label>
-                      <div className={styles.imgsvg}>
-                        <img
-                          className={styles.Google}
-                          src={Google}
-                          alt="Geogle"
-                        />
-                        <img
-                          className={styles.facebook}
-                          src={facebook}
-                          alt="facebook"
-                        />
-                        <img
-                          className={styles.yandex}
-                          src={yandex}
-                          alt="yandex"
-                        />
-                      </div>
-                    </div>
-                  </form>
-                </div>
+                  </div>
+                </form>
               </div>
             </div>
           </div>
         </div>
-      </Container>
-    );
-  };
+      </div>
+    </Container>
+  );
+};
 
-  export { Auth };
-
-  export { Auth };
+export { Auth };
