9c2b1423207d69ca816b1c6b2843b6f0b97fb742
ConstantinKapanevs <2053240@mail.ru>
2023-06-29 10:27:09 +0300

    Fix account info

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -29,11 +29,12 @@ function Header() {
 
   useEffect(() => {
     authControl(localStorage.getItem("TOKEN"), localStorage.getItem("EXPIRE"));
+    setUserName(localStorage.getItem("User"));
     if (isAuth) {
       getInfoData(token);
       console.log("nice");
     }
-  }, [isAuth, location]);
+  }, [isAuth, userName, location]);
 
   function authControl(token, expireDate) {
     if (token && expireDate) {
