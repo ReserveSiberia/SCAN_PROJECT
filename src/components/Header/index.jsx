716e5f33da44319680a44886f09e886b475c7fef
ConstantinKapanevs <2053240@mail.ru>
2023-07-03 11:27:32 +0300

    Minor changes

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,6 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 import store from "../../store/store.js";
 import styles from "./Header.module.css";
 import BurgerMenu from "../../components/BurgerMenu/BurgerMenu.jsx";
-import NavBar from "../NavBar/NavBar.jsx";
 import Logo from "../../assets/images/Logo-image.svg";
 import LogoInverted from "../../assets/images/Logo-image-inverted.svg";
 import Photo from "../../assets/images/Avatar4.jpg";
