9d3d476a30c7c3cfbac15b40864e7ac60870eb6e
Roman__Mal'cev <61384075+DevFromSiberia@users.noreply.github.com>
2023-07-03 22:21:59 +0700

    comleted parse xml function

--- src/utils/parseXml.js
+++ src/utils/parseXml.js
@@ -1,6 +1,13 @@
 export function parseXml(xml) {
   const parser = new DOMParser()
-  const html = parser.parseFromString(xml, "application/xml")
-  console.log(html)
-  return html
+  const html = parser.parseFromString(xml, "text/xml")
+  let result = ''
+  html.getElementsByTagName("scandoc")[0].childNodes.forEach((node, index) => {
+    if (index < 3) {
+      if ((node.innerHTML).match(/[а-я ]/gi)) {
+        result += (node.innerHTML).match(/[а-я ]/gi).join('')
+      }
+    }
+  })
+  return result + '...'
 }
