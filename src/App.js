72d32cc5938794efac8d5078873c7b61ac3844f3
Roman__Mal'cev <61384075+DevFromSiberia@users.noreply.github.com>
2023-07-04 19:50:01 +0700

    fix bug

--- src/App.js
+++ src/App.js
@@ -10,10 +10,8 @@ import { AuthErrorPage } from "./pages/AuthErrorPage";
 import { SearchPage } from "./pages/SearchPage";
 
 import { Navigate, Outlet, Route, Routes } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { ResultProvider } from "./context/resultProvider";
-
-const fakeArr = [1, 2, 3, 4];
 
 function App() {
   const [isAuth, setIsAuth] = useState(false);
@@ -24,9 +22,9 @@ function App() {
         <Route path="/" element={<MainPage />} />
         <Route
           path="/auth"
-          element={<Auth isAuth={isAuth} setIsAuth={setIsAuth} />}
+          element={!isAuth ? <Auth isAuth={isAuth} setIsAuth={setIsAuth} /> : <Navigate to="/" />}
         />
-        {/* Если пользователь не авторизован то редиректит на форму авторизации, иначе есть доступ к поиску и результатам */}
+        {/* Страницы доступные только авторизованному пользователю */}
         <Route element={isAuth ? <Outlet /> : <Navigate to="/auth" />}>
           <Route path='/search' element={<ResultProvider><SearchPage /></ResultProvider>} />
           <Route path='/result' element={<ResultProvider><ResultPage /></ResultProvider>} />
